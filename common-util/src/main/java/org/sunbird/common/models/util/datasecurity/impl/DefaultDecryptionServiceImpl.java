cd0be10e612b1c87cff04e3382f55036832f6b55
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-06-05 23:50:52 +0530

    Issue #000 fix: Base64 encoding for encrypt and decrypt.

--- common-util/src/main/java/org/sunbird/common/models/util/datasecurity/impl/DefaultDecryptionServiceImpl.java
+++ common-util/src/main/java/org/sunbird/common/models/util/datasecurity/impl/DefaultDecryptionServiceImpl.java
@@ -1,22 +1,19 @@
 /** */
 package org.sunbird.common.models.util.datasecurity.impl;
 
-import java.security.Key;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.PropertiesCache;
 import org.sunbird.common.models.util.datasecurity.DecryptionService;
 
 /** @author Manzarul */
 public class DefaultDecryptionServiceImpl implements DecryptionService {
-  private static String sunbird_encryption = "";
 
   private String sunbirdEncryption = "";
 
@@ -29,50 +26,45 @@ public class DefaultDecryptionServiceImpl implements DecryptionService {
 
   @Override
   public Map<String, Object> decryptData(Map<String, Object> data) {
-//    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
-//      if (data == null) {
-//        return data;
-//      }
-//      Iterator<Entry<String, Object>> itr = data.entrySet().iterator();
-//      while (itr.hasNext()) {
-//        Entry<String, Object> entry = itr.next();
-//        if (!(entry.getValue() instanceof Map || entry.getValue() instanceof List)
-//            && null != entry.getValue()) {
-//          data.put(entry.getKey(), decrypt(entry.getValue() + ""));
-//        }
-//      }
-//    }
+    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
+      if (data == null) {
+        return data;
+      }
+      Iterator<Entry<String, Object>> itr = data.entrySet().iterator();
+      while (itr.hasNext()) {
+        Entry<String, Object> entry = itr.next();
+        if (!(entry.getValue() instanceof Map || entry.getValue() instanceof List)
+            && null != entry.getValue()) {
+          data.put(entry.getKey(), decrypt(entry.getValue() + ""));
+        }
+      }
+    }
     return data;
   }
 
   @Override
   public List<Map<String, Object>> decryptData(List<Map<String, Object>> data) {
-//    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
-//      if (data == null || data.isEmpty()) {
-//        return data;
-//      }
-//
-//      for (Map<String, Object> map : data) {
-//        decryptData(map);
-//      }
-//    }
+    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
+      if (data == null || data.isEmpty()) {
+        return data;
+      }
+
+      for (Map<String, Object> map : data) {
+        decryptData(map);
+      }
+    }
     return data;
   }
 
   @Override
   public String decryptData(String data) {
-//    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
-//      if (StringUtils.isBlank(data)) {
-//        return data;
-//      }
-//      if (null != data) {
-//        return decrypt(data);
-//      } else {
-//        return data;
-//      }
-//    } else {
-//      return data;
-//    }
+    if (JsonKey.ON.equalsIgnoreCase(sunbirdEncryption)) {
+      if (StringUtils.isBlank(data)) {
+        return data;
+      } else {
+    	  	return decrypt(data);
+      }
+    }
     return data;
   }
 
@@ -83,28 +75,8 @@ public class DefaultDecryptionServiceImpl implements DecryptionService {
    * @return decrypted password.
    */
   public static String decrypt(String value) {
-//    try {
-//      sunbird_encryption = DefaultEncryptionServivceImpl.getSalt();
-//      Key key = generateKey();
-//      Cipher c = Cipher.getInstance(ALGORITHM);
-//      c.init(Cipher.DECRYPT_MODE, key);
-//
-//      String dValue = null;
-//      String valueToDecrypt = value.trim();
-//      for (int i = 0; i < ITERATIONS; i++) {
-//        byte[] decordedValue = new sun.misc.BASE64Decoder().decodeBuffer(valueToDecrypt);
-//        byte[] decValue = c.doFinal(decordedValue);
-//        dValue = new String(decValue).substring(sunbird_encryption.length());
-//        valueToDecrypt = dValue;
-//      }
-//      return dValue;
-//    } catch (Exception ex) {
-//      ProjectLogger.log("Exception Occurred while decrypting value");
-//    }
-    return value;
-  }
-
-  private static Key generateKey() {
-    return new SecretKeySpec(keyValue, ALGORITHM);
+	  byte[] bytes = value.getBytes();
+	  byte[] array = Base64.decodeBase64(bytes);
+	  return new String(array);
   }
 }
